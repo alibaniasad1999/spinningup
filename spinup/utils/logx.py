64ab25cdad9b3aa1b2c734345a6edfa3e874c8a6
Ali BaniAsad <37424536+alibaniasad1999@users.noreply.github.com>
2024-08-29 16:05:21 +0330

    Update logx.py delete spinning up needed code

--- spinup/utils/logx.py
+++ spinup/utils/logx.py
@@ -13,8 +13,6 @@ import tensorflow as tf
 import torch
 import os.path as osp, time, atexit, os
 import warnings
-from spinup.utils.mpi_tools import proc_id, mpi_statistics_scalar
-from spinup.utils.serialization_utils import convert_json
 
 color2num = dict(
     gray=30,
