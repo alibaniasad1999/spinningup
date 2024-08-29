d63572d9781bd640a05ac5788b5f455e8012c500
Ali BaniAsad <37424536+alibaniasad1999@users.noreply.github.com>
2024-08-29 16:59:08 +0330

    Update logx.py

--- spinup/utils/logx.py
+++ spinup/utils/logx.py
@@ -13,8 +13,8 @@ import tensorflow as tf
 import torch
 import os.path as osp, time, atexit, os
 import warnings
-from mpi_tools import proc_id, mpi_statistics_scalar
-from serialization_utils import convert_json
+from utils.mpi_tools import proc_id, mpi_statistics_scalar
+from utils.serialization_utils import convert_json
 
 color2num = dict(
     gray=30,
